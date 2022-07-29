f77fd2f9a4e2b4d9c1c2ec0734419c6903cb8e29
James Alhassan <jamesalhassan052@gmail.com>
2022-07-29 22:15:37 +0100

    Overlay

--- app/script.js
+++ app/script.js
@@ -1,3 +1,4 @@
+// ELEMENT SELECTOR FUNCTION
 function getElement(selection) {
   const element = document.querySelector(selection);
   if (element) {
@@ -7,13 +8,17 @@ function getElement(selection) {
   }
 }
 
+const overlay = getElement("#overlay");
+
 const toggleBars = getElement(".toggle-bars");
 toggleBars.addEventListener("click", () => {
   toggleBars.classList.add("hide-toggle-bars");
   closeBtn.classList.add("show-close-btn");
+  overlay.classList.toggle("overlay");
 });
 const closeBtn = getElement(".close-btn");
 closeBtn.addEventListener("click", () => {
   toggleBars.classList.remove("hide-toggle-bars");
   closeBtn.classList.remove("show-close-btn");
+  overlay.classList.toggle("overlay");
 });
