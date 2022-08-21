815677986cf9f64b4a905e51030e780b367e5f42
James Alhassan <jamesalhassan052@gmail.com>
2022-08-21 21:47:22 +0000

    no-scroll class added to boby when mobile view toggled is cllicked

--- app/script.js
+++ app/script.js
@@ -10,18 +10,21 @@ function getElement(selection) {
 
 const overlay = getElement("#overlay"); //
 const mbNavLinks = getElement(".mb-Navlinks"); //select mobile nav links div
-
+const body = getElement(".body");
 const toggleBars = getElement(".toggle-bars");
+
 toggleBars.addEventListener("click", () => {
   toggleBars.classList.add("hide-toggle-bars");
   closeBtn.classList.add("show-close-btn");
-  overlay.classList.toggle("overlay");
-  mbNavLinks.classList.toggle("mb-Navlinks-show");
+  overlay.classList.add("overlay");
+  mbNavLinks.classList.add("mb-Navlinks-show");
+  body.classList.add("no-scroll");
 });
 const closeBtn = getElement(".close-btn");
 closeBtn.addEventListener("click", () => {
   toggleBars.classList.remove("hide-toggle-bars");
   closeBtn.classList.remove("show-close-btn");
-  overlay.classList.toggle("overlay");
-  mbNavLinks.classList.toggle("mb-Navlinks-show");
+  overlay.classList.remove("overlay");
+  mbNavLinks.classList.remove("mb-Navlinks-show");
+  body.classList.remove("no-scroll");
 });
